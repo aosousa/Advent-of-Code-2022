f5d3d51e773dabbacd4320e5d9a6b32d73bcf481
André Sousa <andreosousa95@gmail.com>
2022-12-08 20:28:01 -0100

    Day 8 part 2 implementation

--- day8/main.py
+++ day8/main.py
@@ -6,6 +6,8 @@ def main():
     part_one_result = 0
     part_two_result = 0
 
+    scenic_scores = []
+
     with open(sys.argv[1], 'r', encoding='utf-8') as f:
         lines = [line.strip() for line in f.readlines()]
 
@@ -17,54 +19,58 @@ def main():
                 
                 if idx != 0 and idx != len(lines) - 1 and count != 0 and count != len(line) - 1:
                     top, right, bottom, left = True, True, True, True
+                    top_blocked_at, right_blocked_at, bottom_blocked_at, left_blocked_at = 0, 0, 0, 0
 
                     # compare against top edge
-                    t = idx - 1 # can be a cause of errors
+                    t = idx - 1
                     while t >= 0:
-                        if (int(value) <= int(lines[t][count])):
+                        top_blocked_at += 1
+
+                        if int(value) <= int(lines[t][count]):
                             top = False
                             break
-                        t -= 1
 
-                    if top:
-                        part_one_result += 1
-                        continue
+                        t -= 1
 
                     # compare against right edge
                     r = count + 1
                     while r < len(line):
+                        right_blocked_at += 1
+
                         if int(value) <= int(line[r]):
                             right = False
                             break
-                        r += 1
 
-                    if right:
-                        part_one_result += 1
-                        continue
+                        r += 1
 
                     # compare against bottom edge
                     b = idx + 1
                     while b < len(line):
+                        bottom_blocked_at += 1
+
                         if int(value) <= int(lines[b][count]):
                             bottom = False
                             break
+
                         b += 1
-                    
-                    if bottom:
-                        part_one_result += 1
-                        continue
 
                     # compare against left edge
                     l = count - 1
                     while l >= 0:
+                        left_blocked_at += 1
+
                         if int(value) <= int(line[l]):
                             left = False
                             break
+
                         l -= 1
 
-                    if left:
+                    if left or bottom or top or right:
                         part_one_result += 1
+                        scenic_scores.append(top_blocked_at * right_blocked_at * bottom_blocked_at * left_blocked_at)
                         continue
+
+    part_two_result = max(scenic_scores)
 
     print('{} trees are visible from outside the grid'.format(part_one_result))
     print('The highest scenic score possible for any tree is: {}'.format(part_two_result))
